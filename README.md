88bb6f179975e0d2c402eaa8b287cbeaab46ccd4
Issac Lopez <lopezissaca@gmail.com>
2022-09-28 13:20:21 -0600

    updates to projects and revisions

--- README.md
+++ README.md
@@ -1,23 +1,22 @@
 # Tasks
 
 ## To-Do:
-- resume pdf linked to website; href element
-- figure out fonts
+- mess around with fonts
 - scroll-behavior alternative using js: https://stackoverflow.com/questions/51229742/javascript-window-scroll-behavior-smooth-not-working-in-safari
 - add more to each section (i.e, pictures, more text)
-- comments for js code
 - add links for resources used
 - custom favicon for the tab in browser
 
 ## In progress:
-- update projects when needed --> add a see all that links to github
+- refactor js
+- update projects when needed --> add a see all that link to github
 - revise/write blog posts and publish
 - optimize to get best overall score in lighthouse for mobile and desktop
 - make page more dynamic
 
 ## Completed:
-- overall ui
-- day-night mode and colros for themes 
+- basic ui
+- day-night mode and colors for themes 
 - email requests
 - links to all socials with icons
 - skills section
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,34 @@
 const body = document.body
-
+// Event Listeners
 const btnTheme = document.querySelector('.fa-moon')
 const btnHamburger = document.querySelector('.fa-bars')
-
+// function to add theme class
 const addThemeClass = (bodyClass, btnClass) => {
-  body.classList.add(bodyClass)
-  btnTheme.classList.add(btnClass)
+	body.classList.add(bodyClass)
+	btnTheme.classList.add(btnClass)
 }
-
+// add event listener to theme button
 const getBodyTheme = localStorage.getItem('portfolio-theme')
 const getBtnTheme = localStorage.getItem('portfolio-btn-theme')
-
+// add event listener to theme button
 addThemeClass(getBodyTheme, getBtnTheme)
-
+//	Scroll to top
 const isDark = () => body.classList.contains('dark')
-
+// function to set theme 
 const setTheme = (bodyClass, btnClass) => {
-
 	body.classList.remove(localStorage.getItem('portfolio-theme'))
 	btnTheme.classList.remove(localStorage.getItem('portfolio-btn-theme'))
-
-  addThemeClass(bodyClass, btnClass)
-
+	addThemeClass(bodyClass, btnClass)
 	localStorage.setItem('portfolio-theme', bodyClass)
 	localStorage.setItem('portfolio-btn-theme', btnClass)
 }
-
+// Event Listeners theme toggle
 const toggleTheme = () =>
 	isDark() ? setTheme('light', 'fa-moon') : setTheme('dark', 'fa-sun')
-
 btnTheme.addEventListener('click', toggleTheme)
-
+// Event Listeners hamburger menu
 const displayList = () => {
 	const navUl = document.querySelector('.nav__list')
-
 	if (btnHamburger.classList.contains('fa-bars')) {
 		btnHamburger.classList.remove('fa-bars')
 		btnHamburger.classList.add('fa-times')
@@ -44,12 +39,11 @@ const displayList = () => {
 		navUl.classList.remove('display-nav-list')
 	}
 }
-
 btnHamburger.addEventListener('click', displayList)
-
+// Scroll to top
 const scrollUp = () => {
 	const btnScrollTop = document.querySelector('.scroll-top')
-
+	// Show scroll top button
 	if (
 		body.scrollTop > 500 ||
 		document.documentElement.scrollTop > 500
@@ -59,5 +53,4 @@ const scrollUp = () => {
 		btnScrollTop.style.display = 'none'
 	}
 }
-
 document.addEventListener('scroll', scrollUp)
